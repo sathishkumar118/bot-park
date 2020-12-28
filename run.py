5da8d093d20114f8bad093c3b40218aa53bf9ba7
Sathish Kumar S <ssathishkumar.118@gmail.com>
2020-12-28 19:11:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 # app.py
 from flask import Flask           # import flask
-app = Flask(__name__)             # create an app instance
+my_awesome_app = Flask(__name__)             # create an app instance
 
-@app.route("/")                   # at the end point /
+@my_awesome_app.route("/")                   # at the end point /
 def hello():                      # call method hello
     return "Hello World!"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+    my_awesome_app.run()                     # run the flask app
